5174b89c4f961ad434c76ef08e80fe8be7b4815c
Joshua Maney <joshuaraymaney@gmail.com>
2022-02-23 18:48:53 -0500

    adding a splash screen with animation. needs a ton of work, but looking cool.

---
+++ index.js
@@ -0,0 +1,9 @@
+// anime({
+//     // targets: '.namedview7 .lines path',
+//     strokeDashoffset: [anime.setDashoffset, 10],
+//     easing: 'easeInOutSine',
+//     duration: 10000,
+//     delay: function(el, i) { return i * 25 },
+//     direction: 'alternate',
+//     loop: false
+//   });
